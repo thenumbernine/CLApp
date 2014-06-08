af1b292f5245c8c5e108bd9c7d3b799b41ea4a5f
Christopher Moore <christopher.e.moore@gmail.com>
2014-06-08 02:11:42 -0700

    added hepler function for setting multiple kernel arguments at once

--- include/CLApp/CLApp.h
+++ include/CLApp/CLApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "GLApp/GLApp.h"
+#include "Common/Meta.h"
 #include <OpenCL/cl.hpp>
 
 namespace CLApp {
@@ -19,6 +20,22 @@ struct CLApp : public ::GLApp::GLApp {
 	virtual cl::Device getDevice(cl::Platform platform);
 	
 	virtual void init();
+
+	template<int index>
+	struct SetArg {
+		template<typename... Args>
+		static bool exec(cl::Kernel kernel, Args... args) {
+			kernel.setArg(index, std::get<index>(std::make_tuple(args...)));
+			return false;
+		}
+	};
+
+	//helper functions
+	template<typename... ArgList>
+	void setArgs(cl::Kernel kernel, ArgList&&... args) {
+		typedef TypeVector<ArgList...> Args;	
+		ForLoop<0, Args::size, SetArg>::exec(kernel, std::forward<ArgList>(args)...);
+	}
 };
 
 };
